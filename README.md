0e587974d27a822c98e1b15779939beef5998169
hiep1810 <hiep981018@gmail.com>
2023-12-29 19:03:01 +0700

    init

---
+++ README.md
@@ -0,0 +1,85 @@
+# FSND capstone Details
+
+-----
+
+## Introduction
+
+### Overview
+
+The Casting Agency models a company that is responsible for creating movies and managing and assigning actors to those movies. You are an Executive Producer within the company and are creating a system to simplify and streamline your process.
+
+- Models:
+    - Movies with attributes title and release date
+    - Actors with attributes name, age and gender
+- Endpoints:
+    - GET /actors and /movies
+    - DELETE /actors/ and /movies/
+    - POST /actors and /movies and
+    - PATCH /actors/ and /movies/
+- Roles:
+    - Casting Assistant
+        - Can view actors and movies
+    - Casting Director
+        - All permissions a Casting Assistant has and…
+        - Add or delete an actor from the database
+        - Modify actors or movies
+    - Executive Producer
+        - All permissions a Casting Director has and…
+        - Add or delete a movie from the database
+- Tests:
+    - One test for success behavior of each endpoint
+    - One test for error behavior of each endpoint
+    - At least two tests of RBAC for each role
+
+## Tech Stack (Dependencies)
+
+### 1. Backend Dependencies
+Our tech stack will include the following:
+ * **A virtual environment** provided in the workspace (if working locally, see README)
+ * **SQLAlchemy ORM** to be our ORM library of choice
+ * **PostgreSQL** as our database of choice
+ * **Python3** and **Flask** as our server language and server framework
+ * **Flask-Migrate** for creating and running schema migrations
+
+## Development Setup
+
+1. **Install the dependencies:**
+```
+pip install -r requirements.txt
+```
+
+2. **Database:**
+
+Log in to an interactive Postgres session using the following command:
+
+```
+sudo -iu postgres psql
+```
+First, create a database for your project:
+```
+CREATE DATABASE filmography;
+```
+Next, create a database user for our project. Make sure to select a secure password:
+```
+CREATE USER admin WITH PASSWORD 'admin';
+```
+
+3. **Run the development server:**
+
+Each time you open a new terminal session, run:
+
+```
+#DATABASE
+export DATABASE_USERNAME='admin'
+export DATABASE_PASSWORD='admin'
+export DATABASE_HOST='localhost'
+export DATABASE_PORT=5432
+export DATABASE_NAME='filmography'
+
+export FLASK_APP=api.py;
+export FLASK_ENV=development
+
+export UTH0_DOMAIN='your_auth0_domain'
+export API_AUDIENCE='auth0_audience'
+```
+
---
+++ database/models.py
@@ -0,0 +1,189 @@
+import os
+from datetime import date
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote_plus
+
+# Replace the values with your actual database information
+username = os.environ['DATABASE_USERNAME']
+password = os.environ['DATABASE_PASSWORD']
+host = os.environ['DATABASE_HOST']
+port = os.environ['DATABASE_PORT']
+database_name = os.environ['DATABASE_NAME']
+
+# Construct the URI
+database_path = f'postgresql://{username}:{quote_plus(password)}@{host}:{port}/{database_name}'
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+    !!NOTE you can change the database_filename variable to have multiple verisons of a database
+'''
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+    # add one demo row which is helping in POSTMAN test
+
+    # Create actors
+    actor1 = Actor(name='Actor 1', age=30, gender='Male')
+    actor2 = Actor(name='Actor 2', age=25, gender='Female')
+    actor3 = Actor(name='Actor 3', age=35, gender='Male')
+    db.session.add_all([actor1, actor2, actor3])
+    db.session.commit()
+
+    # Create movies
+    movie1 = Movie(title='Movie 1', release_date=date(2022, 1, 1))
+    movie2 = Movie(title='Movie 2', release_date=date(2023, 2, 15))
+    movie3 = Movie(title='Movie 3', release_date=date(2024, 5, 10))
+    db.session.add_all([movie1, movie2, movie3])
+    db.session.commit()
+
+    # Associate actors with movies
+    movie1.actors.extend([actor1, actor2])
+    movie2.actors.extend([actor2, actor3])
+    movie3.actors.append(actor3)
+    db.session.commit()
+
+
+# Association table for the many-to-many relationship between actors and movies
+ActorMovie = db.Table('ActorMovie',
+    db.Column('actor_id', db.Integer, db.ForeignKey('Actor.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('movie_id', db.Integer, db.ForeignKey('Movie.id', ondelete='CASCADE'), primary_key=True)
+)
+
+
+'''
+Movie
+a persistent Movie entity, extends the base SQLAlchemy Model
+'''
+class Movie(db.Model):
+    __tablename__ = 'Movie'
+    # Autoincrementing, unique primary key
+    id = db.Column(db.Integer, primary_key=True)
+    # String Title
+    title = db.Column(db.String(128), nullable=False)
+    # Release Date
+    release_date = db.Column(db.Date, nullable=False)
+    # Define the relationship with the Actor table through the ActorMovie table 
+    actors = db.relationship('Actor', \
+                             secondary=ActorMovie, \
+                             backref='movies')
+
+    '''
+    insert()
+        inserts a new model into a database
+        the model must have a unique name
+        the model must have a unique id or null id
+        EXAMPLE
+            movie = Movie(title=req_title, release_date=req_release_date)
+            movie.insert()
+    '''
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            movie.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+    update()
+        updates a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            movie = Movie.query.filter(Movie.id == id).one_or_none()
+            movie.title = 'Movie 1'
+            movie.update()
+    '''
+    def update(self):
+        db.session.commit()
+
+    # Define the string representation of a Movie object
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date.strftime('%Y-%m-%d'),
+            'actors': [actor.serialize() for actor in self.actors]
+        }
+'''
+Actor
+a persistent Actor entity, extends the base SQLAlchemy Model
+'''
+class Actor(db.Model):
+    __tablename__ = 'Actor'
+    # Autoincrementing, unique primary key
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(16), nullable=False)
+    '''
+    insert()
+        inserts a new actor into a database
+        the actor must have a unique name
+        the actor must have a unique id or null id
+        EXAMPLE
+            actor = Actor(name=req_name, age=req_age, gender=req_gender)
+            actor.insert()
+    '''
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a new actor into a database
+        the actor must exist in the database
+        EXAMPLE
+            actor.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    '''
+    update()
+        updates a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            actor = Actor.query.filter(Actor.id == id).one_or_none()
+            actor.name = 'Actor 1'
+            actor.update()
+    '''
+    def update(self):
+        db.session.commit()
+
+    # Define the string representation of a Actor object
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
